5673a471c359543c911d3c36f12f739f6c1a71df
Juan <pront23@gmail.com>
2022-01-06 15:03:08 +0900

    added routing

---
+++ components/Nav/Navbar.js
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import router from 'next/router';
+
+import styles from './Navbar.module.css';
+
+const Navbar = ({ username }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>Netflix</div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem} onClick={() => router.push('/')}>
+            Home
+          </li>
+          <li
+            className={styles.navItem2}
+            onClick={() => router.push('/browse/my-list')}
+          >
+            My List
+          </li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+            </button>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <Link href="/login">
+                  <a className={styles.linkName}>Sign out</a>
+                </Link>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
