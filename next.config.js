2218db8290a8a145a25848a83f16070386befea6
Juan <pront23@gmail.com>
2022-01-08 21:25:54 +0900

    updated config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['images.unsplash.com', 'i.ytimg.com'],
+    domains: ['images.unsplash.com', 'i.ytimg.com', 'yt3.ggpht.com'],
   },
   reactStrictMode: true,
 };
