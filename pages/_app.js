57b9468ab595f38a517082cf29e60a17429629d9
Juan <pront23@gmail.com>
2022-01-11 18:02:20 +0900

    reduce flicker on route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useRouter } from 'next/router';
 
@@ -7,6 +7,8 @@ import { magic } from '../lib/magic-client';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const [isLoading, setIsLoading] = useState(true);
+
   const router = useRouter();
   // if logged in
   // route to /
@@ -23,7 +25,19 @@ function MyApp({ Component, pageProps }) {
     }
   }, []);
 
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    const handleComplete = () => setIsLoading(false);
+
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router]);
+
+  return isLoading ? <div>Loading...</div> : <Component {...pageProps} />;
 }
 
 export default MyApp;
