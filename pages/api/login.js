9d1a9de7ea5ed90d2ddca0a96761868e6af1ced5
Juan <pront23@gmail.com>
2022-01-26 17:37:21 +0900

    created basic login api

---
+++ pages/api/login.js
@@ -0,0 +1,15 @@
+export default async function login(req, res) {
+  if (req.method == 'POST') {
+    try {
+      const auth = req.headers.authorization;
+      const token = auth ? auth.substr(7) : '';
+      console.log({ token });
+      res.status(200).send({ done: true });
+    } catch (error) {
+      console.error('Something went wrong logging in', error);
+      res.status(500).send({ done: false });
+    }
+  } else {
+    res.send({ done: false });
+  }
+}
