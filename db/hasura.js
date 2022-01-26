f21331ca4cb444202db70d80e4f1c4a7c624644e
Juan <pront23@gmail.com>
2022-01-26 22:41:37 +0900

    added check user and query functions

--- db/hasura.js
+++ db/hasura.js
@@ -1,12 +1,37 @@
+export async function isNewUser(token) {
+  const operationsDoc = `
+  query MyQuery {
+    users(where:{issuer:{_eq:
+    ''
+    }})
+    {
+      id
+      email
+      issuer
+    }
+  }
+`;
+  const response = await queryHasuraGraphQl(
+    operationsDoc,
+    'MyQuery',
+    {},
+    token
+  );
+  console.log({ response });
+  return response?.users?.length === 0;
+}
+
 export async function queryHasuraGraphQl(
   operationsDoc,
   operationName,
-  variables
+  variables,
+  token
 ) {
   const result = await fetch(process.env.NEXT_PUBLIC_HASURA_ADMIN_URL, {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
+      Authorization: `Bearer ${token}`,
       'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_ADMIN_KEY,
     },
     body: JSON.stringify({
