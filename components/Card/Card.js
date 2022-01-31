1970ae09104cbd506695812c6f697ddcd932a498
Juan Perez <jperezdev@vivaldi.net>
2022-01-31 16:26:46 +0900

    added hnadleOnError function

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -17,6 +17,12 @@ const Card = ({ id, imgUrl, size = 'medium' }) => {
 
   const scale = id === 0 ? { scaleY: 1.1 } : { scale: 1.1 };
 
+  const handleOnError = () => {
+    setImgSrc(
+      'https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1925&q=80'
+    );
+  };
+
   return (
     <div className={styles.container}>
       <motion.div
@@ -24,15 +30,11 @@ const Card = ({ id, imgUrl, size = 'medium' }) => {
         whileHover={{ ...scale }}
       >
         <Image
-          className={styles.cardImg}
           src={imgSrc}
-          alt="large image"
+          onError={handleOnError}
+          alt="image"
           layout="fill"
-          onError={() =>
-            setImgSrc(
-              'https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1925&q=80'
-            )
-          }
+          className={styles.cardImg}
         />
       </motion.div>
     </div>
