66aab05d65c0d8724936d86eb99ced59c6fbd2ae
Juan Perez <jperezdev@vivaldi.net>
2022-01-31 13:20:36 +0900

    update stats api added inputParams const

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -12,7 +12,9 @@ export default async function stats(req, res) {
     if (!token) {
       res.status(403).send({ msg: 'Serverzugriff Verboten!' });
     } else {
-      const { videoId = true } = req.body;
+      const inputParams = req.method === 'POST' ? req.body : req.query;
+
+      const { videoId } = inputParams;
 
       if (videoId) {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
