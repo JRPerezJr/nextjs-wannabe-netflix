79f31b3e936701b9e0bd07fa16f5fcdf00cf9ccb
Juan Perez <jperezdev@vivaldi.net>
2022-01-31 15:23:04 +0900

    cleaned code

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,11 @@ import {
 import styles from '../styles/Home.module.css';
 
 export async function getServerSideProps() {
-  const userId = 'did:ethr:0x9B5178e5C094f29faD1083e79d76765E48778F17';
-  const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZXIiOiJkaWQ6ZXRocjoweDlCNTE3OGU1QzA5NGYyOWZhRDEwODNlNzlkNzY3NjVFNDg3NzhGMTciLCJwdWJsaWNBZGRyZXNzIjoiMHg5QjUxNzhlNUMwOTRmMjlmYUQxMDgzZTc5ZDc2NzY1RTQ4Nzc4RjE3IiwiZW1haWwiOiJwcm9udDIzQGdtYWlsLmNvbSIsIm9hdXRoUHJvdmlkZXIiOm51bGwsInBob25lTnVtYmVyIjpudWxsLCJpYXQiOjE2NDM1OTEyNTgsImV4cCI6MTY0NDE5NjA1OCwiaHR0cHM6Ly9oYXN1cmEuaW8vand0L2NsYWltcyI6eyJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiLCJhZG1pbiJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwieC1oYXN1cmEtdXNlci1pZCI6ImRpZDpldGhyOjB4OUI1MTc4ZTVDMDk0ZjI5ZmFEMTA4M2U3OWQ3Njc2NUU0ODc3OEYxNyJ9fQ.7qtSdsNKM7oXf8jjKqP3l94D4cNKBmtyqZsd0FRI-WQ';
+  // const userId = '';
+  // const token = '';
 
-  const watchItAgainVideos = await getWatchItAgainVideos(userId, token);
-  console.log({ watchItAgainVideos });
+  // const watchItAgainVideos = await getWatchItAgainVideos(userId, token);
+  // console.log({ watchItAgainVideos });
 
   // query and etag
   const disneyVideos = await getVideos(
