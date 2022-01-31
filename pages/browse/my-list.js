fffad30f98aa5e965e6b2fb07e896cc4431ab9b3
Juan Perez <jperezdev@vivaldi.net>
2022-01-31 21:33:48 +0900

    completed My List page

---
+++ pages/browse/my-list.js
@@ -0,0 +1,49 @@
+import Head from 'next/head';
+import Layout from '../../components/Layout';
+
+import SectionCards from '../../components/SectionCards/SectionCards';
+import { getMyVideoList } from '../../lib/videos';
+
+import styles from '../../styles/MyList.module.css';
+import useRedirectUser from '../../utils/redirectUser';
+
+export async function getServerSideProps(context) {
+  const { userId, token } = await useRedirectUser(context);
+  //   if (!userId) {
+  //     return {
+  //       props: {},
+  //       redirect: {
+  //         destination: '/login',
+  //         permanent: false,
+  //       },
+  //     };
+  //   }
+
+  const myVideos = await getMyVideoList(userId, token);
+  return {
+    props: {
+      myVideoList: myVideos,
+    },
+  };
+}
+
+const MyList = ({
+  myVideoList,
+  title = 'My List',
+  description = 'My favorites video list',
+}) => {
+  return (
+    <Layout title={title}>
+      <Head>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.sectionWrapper}>
+          <SectionCards title={title} videos={myVideoList} size="small" />
+        </div>
+      </main>
+    </Layout>
+  );
+};
+
+export default MyList;
