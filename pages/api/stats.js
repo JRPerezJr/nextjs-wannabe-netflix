cafcebf3969b55d9241a23f3f4226e2739a1812a
Juan Perez <jperezdev@vivaldi.net>
2022-01-31 11:37:45 +0900

    updated findVideoByUser function

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -20,12 +20,9 @@ export default async function stats(req, res) {
 
           const userId = decodedToken.issuer;
 
-          const doesStatsExist = await findVideoIdByUser(
-            token,
-            userId,
-            videoId
-          );
+          const findVideo = await findVideoIdByUser(token, userId, videoId);
 
+          const doesStatsExist = findVideo?.length > 0;
           if (doesStatsExist) {
             const response = await updateStatsByUserId(token, {
               watched,
@@ -45,14 +42,41 @@ export default async function stats(req, res) {
             res.status(200).send({ data: response });
           }
         }
-
-        // res.status(200).send({ decodedToken, findVideoId });
       }
     } catch (error) {
       console.log('Error occurred /stats', error);
       res.status(500).send({ done: false, error: error?.message });
     }
   } else {
+    try {
+      const token = req.cookies.token;
+
+      if (!token) {
+        res.status(403).send({ msg: 'Serverzugriff Verboten!' });
+      } else {
+        const { videoId } = req.body;
+
+        if (videoId) {
+          const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+          const userId = decodedToken.issuer;
+
+          const findVideo = await findVideoIdByUser(token, userId, videoId);
+
+          const doesStatsExist = findVideo?.length > 0;
+          if (doesStatsExist) {
+            res.status(200).send(findVideo);
+          } else {
+            res.status(404).send({ user: null, msg: 'Video not found' });
+          }
+        } else {
+          res.status(500).send({ msg: 'videoId is required' });
+        }
+      }
+    } catch (error) {
+      console.log('Error occurred /stats', error);
+      res.status(500).send({ done: false, error: error?.message });
+    }
     res.status(400).send({ msg: 'Your request failed!' });
   }
 }
